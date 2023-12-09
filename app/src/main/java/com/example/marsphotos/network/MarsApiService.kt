a31a84f16816398cb391afa5d6559d57ed6f4b01
ngthgph <ngthgph112@gmail.com>
2023-12-09 19:36:45 +0700

    Add suspend and change to https in url

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -5,14 +5,14 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL =
-    "http://android-kotlin-fun-mars-server.appspot.com"
+    "https://android-kotlin-fun-mars-server.appspot.com"
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 interface MarsApiService {
     @GET("photos")
-    fun getPhotos(): String
+    suspend fun getPhotos(): String
 }
 object MarsApi {
     val retrofitService: MarsApiService by lazy {
